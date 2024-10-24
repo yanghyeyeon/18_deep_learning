d83243fe622ac4ecda0a3e5b543107200b7c21b6
yanghyeyeon <yhy4058@gmail.com>
2024-10-24 14:35:29 +0900

    03 serving

--- 03_serving/main.py
+++ 03_serving/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from routers import translate
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(translate.router)
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, port=8000)
+
+# uvicorn main:app --reload --port=8000  포트 바꾸는 방법
